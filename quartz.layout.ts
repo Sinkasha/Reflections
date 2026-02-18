ff6631b2f8bf304144b7e9a1edfe8edea4f342b5
Sinkasha <xusherri@gmail.com>
2026-02-17 20:15:15 -0800

    Quartz sync: Feb 17, 2026, 8:15 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,12 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-        { 
-          Component: Component., 
-          justify: "start"
-        },
-        
+        { Component: Component.ReaderMode() },        
       ],
     }),
     Component.Explorer(),

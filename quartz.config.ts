0b41160b38f97182695ca0e4b1d686988af67361
Sinkasha <xusherri@gmail.com>
2026-02-17 18:45:44 -0800

    Quartz sync: Feb 17, 2026, 6:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "sinkasha.github.io/Reflections/",
+    baseUrl: "sinkasha.github.io/Reflections",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
